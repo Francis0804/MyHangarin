5e66496da87fa151eb7726ca45f2f54cf0d4b1a7
Francis0804 <sicnarfaying.307@gmail.com>
2025-10-15 01:26:53 +0800

    yehey done part 4

--- MyHangarinsite/MyHangarinorg/views.py
+++ MyHangarinsite/MyHangarinorg/views.py
@@ -1,11 +1,128 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
+from django.urls import reverse_lazy
+from .models import Task, SubTask, Note, Category, Priority
+from .forms import TaskForm, SubTaskForm, NoteForm, CategoryForm, PriorityForm
 
 class HomePageView(TemplateView):
     template_name = "home.html"
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Add any context data you want to pass to the template
-        context['welcome_message'] = "Welcome to Hangarin Task Manager"
-        return context
+
+# Task Views
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'task_list.html'
+    paginate_by = 10
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'task_confirm_delete.html'
+    success_url = reverse_lazy('task-list')
+
+# SubTask Views
+class SubTaskList(ListView):
+    model = SubTask
+    context_object_name = 'subtasks'
+    template_name = 'subtask_list.html'
+    paginate_by = 10
+
+class SubTaskCreateView(CreateView):
+    model = SubTask
+    form_class = SubTaskForm
+    template_name = 'subtask_form.html'
+    success_url = reverse_lazy('subtask-list')
+
+class SubTaskUpdateView(UpdateView):
+    model = SubTask
+    form_class = SubTaskForm
+    template_name = 'subtask_form.html'
+    success_url = reverse_lazy('subtask-list')
+
+class SubTaskDeleteView(DeleteView):
+    model = SubTask
+    template_name = 'subtask_confirm_delete.html'
+    success_url = reverse_lazy('subtask-list')
+
+# Note Views
+class NoteList(ListView):
+    model = Note
+    context_object_name = 'notes'
+    template_name = 'note_list.html'
+    paginate_by = 10
+
+class NoteCreateView(CreateView):
+    model = Note
+    form_class = NoteForm
+    template_name = 'note_form.html'
+    success_url = reverse_lazy('note-list')
+
+class NoteUpdateView(UpdateView):
+    model = Note
+    form_class = NoteForm
+    template_name = 'note_form.html'
+    success_url = reverse_lazy('note-list')
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    template_name = 'note_confirm_delete.html'
+    success_url = reverse_lazy('note-list')
+
+# Category Views
+class CategoryList(ListView):
+    model = Category
+    context_object_name = 'categories'
+    template_name = 'category_list.html'
+    paginate_by = 10
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category_form.html'
+    success_url = reverse_lazy('category-list')
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category_form.html'
+    success_url = reverse_lazy('category-list')
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category_confirm_delete.html'
+    success_url = reverse_lazy('category-list')
+
+# Priority Views
+class PriorityList(ListView):
+    model = Priority
+    context_object_name = 'priorities'
+    template_name = 'priority_list.html'
+    paginate_by = 10
+
+class PriorityCreateView(CreateView):
+    model = Priority
+    form_class = PriorityForm
+    template_name = 'priority_form.html'
+    success_url = reverse_lazy('priority-list')
+
+class PriorityUpdateView(UpdateView):
+    model = Priority
+    form_class = PriorityForm
+    template_name = 'priority_form.html'
+    success_url = reverse_lazy('priority-list')
+
+class PriorityDeleteView(DeleteView):
+    model = Priority
+    template_name = 'priority_confirm_delete.html'
+    success_url = reverse_lazy('priority-list')
--- MyHangarinsite/MyHangarinsite/urls.py
+++ MyHangarinsite/MyHangarinsite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for MyHangarinsite project.
+URL configuration for projectsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,9 +16,46 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from MyHangarinorg.views import HomePageView
+from MyHangarinorg.views import (
+    HomePageView, 
+    TaskList, TaskCreateView, TaskUpdateView, TaskDeleteView,
+    SubTaskList, SubTaskCreateView, SubTaskUpdateView, SubTaskDeleteView,
+    NoteList, NoteCreateView, NoteUpdateView, NoteDeleteView,
+    CategoryList, CategoryCreateView, CategoryUpdateView, CategoryDeleteView,
+    PriorityList, PriorityCreateView, PriorityUpdateView, PriorityDeleteView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    
+    # Task URLs
+    path('tasks/', TaskList.as_view(), name='task-list'),
+    path('tasks/add/', TaskCreateView.as_view(), name='task-add'),
+    path('tasks/<int:pk>/edit/', TaskUpdateView.as_view(), name='task-update'),
+    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+    
+    # SubTask URLs
+    path('subtasks/', SubTaskList.as_view(), name='subtask-list'),
+    path('subtasks/add/', SubTaskCreateView.as_view(), name='subtask-add'),
+    path('subtasks/<int:pk>/edit/', SubTaskUpdateView.as_view(), name='subtask-update'),
+    path('subtasks/<int:pk>/delete/', SubTaskDeleteView.as_view(), name='subtask-delete'),
+    
+    # Note URLs
+    path('notes/', NoteList.as_view(), name='note-list'),
+    path('notes/add/', NoteCreateView.as_view(), name='note-add'),
+    path('notes/<int:pk>/edit/', NoteUpdateView.as_view(), name='note-update'),
+    path('notes/<int:pk>/delete/', NoteDeleteView.as_view(), name='note-delete'),
+    
+    # Category URLs
+    path('categories/', CategoryList.as_view(), name='category-list'),
+    path('categories/add/', CategoryCreateView.as_view(), name='category-add'),
+    path('categories/<int:pk>/edit/', CategoryUpdateView.as_view(), name='category-update'),
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
+    
+    # Priority URLs
+    path('priorities/', PriorityList.as_view(), name='priority-list'),
+    path('priorities/add/', PriorityCreateView.as_view(), name='priority-add'),
+    path('priorities/<int:pk>/edit/', PriorityUpdateView.as_view(), name='priority-update'),
+    path('priorities/<int:pk>/delete/', PriorityDeleteView.as_view(), name='priority-delete'),
 ]
